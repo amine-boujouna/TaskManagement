0ecb7aeebe8b2f6b8d0e60b0775cee9d5a23b3c3
amine boujouna <amine.boujouna@esprit.tn>
2024-12-18 11:53:14 +0100

    modifiertache

--- src/main/java/com/example/taskmanagement/controller/TacheController.java
+++ src/main/java/com/example/taskmanagement/controller/TacheController.java
@@ -5,10 +5,7 @@ import com.example.taskmanagement.serivce.TacheService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/tasks")
@@ -19,9 +16,20 @@ public class TacheController {
     public TacheController(TacheService tacheService) {
         this.tacheService = tacheService;
     }
+
     @PostMapping()
     public ResponseEntity<Tache> ajouterTache(@RequestBody Tache tache) {
         Tache nouvelleTache = tacheService.ajouterTache(tache);
         return new ResponseEntity<>(nouvelleTache, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Tache> modifierTache(@RequestBody Tache tache, @PathVariable("id") Long id) {
+        try {
+            Tache tache1 = tacheService.modifierTache(id, tache);
+            return ResponseEntity.ok(tache1);
+        } catch (RuntimeException e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
--- src/main/java/com/example/taskmanagement/serivce/TacheService.java
+++ src/main/java/com/example/taskmanagement/serivce/TacheService.java
@@ -5,6 +5,8 @@ import com.example.taskmanagement.repository.TacheRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class TacheService {
     private final TacheRepository tacheRepository;
@@ -15,5 +17,20 @@ public class TacheService {
     }
     public Tache ajouterTache(Tache tache) {
         return tacheRepository.save(tache);
+    }
+    public Tache modifierTache(long id,Tache tache){
+       Optional<Tache> optionalTache = tacheRepository.findById(id);
+       if(optionalTache.isPresent()){
+           Tache existtache = optionalTache.get();
+           existtache.setDatedebut(tache.getDatedebut());
+           existtache.setDatefin(tache.getDatefin());
+           existtache.setDescription(tache.getDescription());
+           existtache.setEtat(tache.getEtat());
+           existtache.setPriorite(tache.getPriorite());
+           existtache.setTitre(tache.getTitre());
+           return tacheRepository.save(existtache);
+       }else{
+           throw new RuntimeException("tache non trouveé avec l'id"+id);
+       }
     }
 }
